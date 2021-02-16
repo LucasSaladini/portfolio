d6738e449b0f53bb76ab9ffe4659b484e8521182
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2021-02-16 16:35:57 -0300

    Master changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,1 +1,30 @@
-import Head from 'next/head';
+import React, { useState } from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click); 
+
+    return (
+        <div>
+            <div className={styles.navbar}>
+                <div className={styles.logo}>
+                    <a href="/">Lucas Saladini</a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Portfólio</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
