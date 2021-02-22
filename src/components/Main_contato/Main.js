b7828a3f490ea1ee96e602b067c5e907f521e5b9
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2021-02-22 10:51:55 -0300

    Added phone number in contact page

--- src/components/Main_contato/Main.js
+++ src/components/Main_contato/Main.js
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Head from 'next/head';
+import Media from 'react-bootstrap/Media';
+import Figure from 'react-bootstrap/Figure';
+import Image from 'react-bootstrap/Image';
+import 'isomorphic-fetch';
 import styles from './Main.module.css';
 
 
@@ -7,13 +11,20 @@ const Main = () => {
     return(
         <main>
             <section className={styles.Intro}>
-                
+                <p>Que tal <strong>entrar em contato</strong> comigo?</p>
+                <p>Você ficou com alguma <strong>dúvida</strong>, quer alguns <strong>esclarecimentos</strong>, ou pedir um <strong>orçamento</strong>?</p>
+                <p>Não hesite em falar comigo! Se você preferir, abaixo tem um <strong>formulário de contato</strong> ou, se quiser, pode falar comigo pelo <strong>WhatsApp</strong></p>
             </section>
-            <section className={styles.technologies}>
+            <section className={styles.whatsapp}>
+                <Image width={50} height={50} className="mr-3" src="/WhatsApp.svg" alt="" />
+                <Media>
+                    <Media.Body>
+                        <h5>(19) 98355 - 7139</h5>
+                    </Media.Body>
+                </Media>
+            </section>
+            <section className={styles.form}>
 
-            </section>
-            <section className={styles.faq}>
-                
             </section>
         </main>
     );
