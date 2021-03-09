bf23d6da6fbf8249ce251eff8e6c7d453e5ab651
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2021-03-09 16:53:46 -0300

    Updated contact page and metas

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,6 +6,9 @@ const Layout = ({ children, title="Lucas Saladini" }) => {
     return (
         <div className={styles.container}>
         <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Portfólio e contato de Lucas Saladini. Webdesigner, Web Dev e App Dev" />
+            <meta name="keywords" content="Webdesign, webdesign, web design, webdesigner, web designer, criador de aplicativos, app dev, web dev, criador, Piracicaba" />
             <title>{ title }</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
